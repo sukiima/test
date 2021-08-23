bce26c99221fcefc629a8e441020e0bcb0cfe91d
sukima <inthesukima@gmail.com>
2021-08-23 08:02:31 +0000

    9-6

---
+++ 1kaime/ch9/9-6/test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	printf("foo bar = %d\n", strcmp("foo", "bar"));
+	printf("bar foo = %d\n", strcmp("bar", "foo"));
+	printf("foo foo = %d\n", strcmp("foo", "foo"));
+	return 0;
+}
+	
