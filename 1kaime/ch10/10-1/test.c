f41c14de00e84c9632da9f6306823769df8afed1
sukima <inthesukima@gmail.com>
2021-08-23 11:13:58 +0000

    10-1

---
+++ 1kaime/ch10/10-1/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char *one_string;
+
+	one_string = malloc(0xffffffffffffffff);
+	if(one_string == NULL) {
+		printf("Memory allocation error!\n");
+	}
+	printf("one_string=%p\n", one_string);
+	free(one_string);
+
+	one_string = malloc(14);
+	if(one_string == NULL) {
+		printf("Memory allocation error!\n");
+		return -1;
+	}
+
+	printf("one_string=%p\n", one_string);
+
+	strncpy(one_string, "hello, world\n", 14 -1);
+	one_string[14-1] = '\0';
+
+	printf("%s", one_string);
+
+	printf("one_string=%p\n", one_string);
+	free(one_string);
+	printf("one_string=%p\n", one_string);
+	return 0;
+}
+
+
