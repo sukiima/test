9b46cd7a8a227468dce1122e41c22d1cd27093e9
sukima <inthesukima@gmail.com>
2021-08-23 07:37:04 +0000

    9-5

---
+++ 1kaime/ch9/9-5/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char filename[256] = "strcat_test";
+	char ext[] = ".c";
+
+	printf("filename is = %s\n", filename);
+
+	strncat(filename, ext, sizeof(filename) - strlen(filename) - 1);
+	filename[sizeof(filename) - 1] = '\n';
+
+	printf("filename is = %s\n", filename);
+
+	return 0;
+}
