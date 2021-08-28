fb10a09240ce0dbb36ccb1484fbf86e685f29646
sukima <inthesukima@gmail.com>
2021-08-28 05:16:15 +0000

    7-16

---
+++ 4kaime/ch7/7-16/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum(int *a, int *b, int *ans){
+	*ans = *a + *b;
+	return 0;
+}
+
+int main()
+{
+	int num_1;
+	int num_2;
+	int *answer;
+	int ans;
+
+	num_1 = 1;
+	num_2 = 2;
+	answer = &ans;
+
+	if(sum(&num_1, &num_2, &ans) != 0){
+		printf("error\n");
+	}
+
+	printf("the answer is \n");
+	printf("answer = %d\n", *answer);
+
+	return 0;
+}
+
