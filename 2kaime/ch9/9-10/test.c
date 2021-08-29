78491e5edf28b4947900d47e116a1ffd726203ba
sukima <inthesukima@gmail.com>
2021-08-29 06:57:46 +0000

    9-10

---
+++ 2kaime/ch9/9-10/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char full_name[256] = "NIPPON,Taro";
+	char first_name[256] = "Hanako";
+	char *ptr;
+
+	ptr = strpbrk(full_name, " ,");
+	ptr++;
+
+	strcpy(ptr, first_name);
+
+	printf("full name = %s\n", full_name);
+
+	return 0;
+}
