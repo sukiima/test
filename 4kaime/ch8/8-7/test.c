2bc5758b0e886bbde434a74c77f57ebfcd917105
sukima <inthesukima@gmail.com>
2021-08-29 04:54:22 +0000

    8-7

---
+++ 4kaime/ch8/8-7/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	struct tag_question {
+		char character;
+		double number;
+	};
+
+	printf("size of question is %zu\n", sizeof(struct tag_question));
+
+	return 0;
+}
