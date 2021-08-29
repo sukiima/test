e4c604443342243e38a4c5a0de7d7da2f427069b
sukima <inthesukima@gmail.com>
2021-08-29 02:44:46 +0000

    8-1

---
+++ 4kaime/ch8/8-1/test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct tag_coord {
+	double x;
+	double y;
+};
+
+int main() {
+	struct tag_coord coord;
+
+	coord.x = 1.00;
+	coord.y = 2.00;
+
+	printf("coord.x = %f\n", coord.x);
+	printf("coord.y = %f\n", coord.y);
+
+	return 0;
+}
