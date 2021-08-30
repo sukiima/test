9523f180d6c2a29235b44124faa99c84ad9466cc
sukima <inthesukima@gmail.com>
2021-08-30 11:54:00 +0000

    9-13

---
+++ 2kaime/ch9/9-13/test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+	char hello[16] = "hello, world";
+	char HELLO[16] = "";
+	char *src, *dst;
+
+	src = hello;
+	dst = HELLO;
+
+	while (*src != '\0') {
+		*dst = toupper(*src);
+		src ++;
+		dst ++;
+	}
+	*dst = '\0';
+
+	printf("%s\n", HELLO);
+
+	return 0;
+}
