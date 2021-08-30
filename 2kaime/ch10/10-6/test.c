19b787b2b4bb17d4d2ee1afe1320ef18e0b0200c
sukima <inthesukima@gmail.com>
2021-08-30 14:31:46 +0000

    10-6

---
+++ 2kaime/ch10/10-6/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+	int a, b;
+	double c, d;
+
+	a = 1000;
+	b = 1000;
+	c = 3.14;
+	d = 3.15;
+
+	if (memcmp(&a, &b, sizeof(int)) == 0) {
+		printf("a and b are the same\n");
+}
+
+	if (memcmp(&c, &d, sizeof(double)) == 0) {
+		printf("c and d are the same\n");
+}
+
+	return 0;
+}
+
