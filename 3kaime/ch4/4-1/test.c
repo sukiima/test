d1f36cf0a50bb1ea00908ed976313af63db7e3bb
sukima <inthesukima@gmail.com>
2021-09-15 12:43:32 +0000

    4-1

---
+++ 3kaime/ch4/4-1/test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+	int a = 0;
+
+	if (a == 0){
+		printf("atteruyo\n");
+	}
+
+	else if(a == 1){
+		printf("machigatteruyo\n");
+	}
+	else if(a == 2){
+		printf("nande\n");
+	}
+	return 0;
+}
+
+
